307432c8558b3ced0b13bdda3c69dcfab3e441d2
CangHai <qc_0f13245b35f84d8bbe62d41e5a117dcd@git.code.tencent.com>
2019-06-25 10:36:13 +0800

    :rocket: Add：新增设计模式模块
    新增设计原则之开闭原则

---
+++ java/design-pattern/src/main/java/com/github/zj/dreamly/design/pattern/design/principle/openclose/ICourse.java
@@ -0,0 +1,12 @@
+package com.github.zj.dreamly.design.pattern.design.principle.openclose;
+
+/**
+ * @author 苍海之南
+ */
+public interface ICourse {
+	Integer getId();
+
+	String getName();
+
+	Double getPrice();
+}
