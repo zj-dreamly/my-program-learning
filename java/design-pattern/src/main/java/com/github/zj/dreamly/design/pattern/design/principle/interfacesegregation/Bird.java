e023dc7a21ee24e1bb9dae474da3bd877eda1d36
CangHai <qc_0f13245b35f84d8bbe62d41e5a117dcd@git.code.tencent.com>
2019-06-25 11:21:57 +0800

    :rocket: Add：新增设计原则之接口隔离原则

---
+++ java/design-pattern/src/main/java/com/github/zj/dreamly/design/pattern/design/principle/interfacesegregation/Bird.java
@@ -0,0 +1,21 @@
+package com.github.zj.dreamly.design.pattern.design.principle.interfacesegregation;
+
+/**
+ * @author 苍海之南
+ */
+public class Bird implements IAnimalAction {
+	@Override
+	public void eat() {
+
+	}
+
+	@Override
+	public void fly() {
+
+	}
+
+	@Override
+	public void swim() {
+
+	}
+}
---
+++ java/design-pattern/src/main/java/com/github/zj/dreamly/design/pattern/design/principle/interfacesegregation/IFlyAnimalAction.java
@@ -0,0 +1,8 @@
+package com.github.zj.dreamly.design.pattern.design.principle.interfacesegregation;
+
+/**
+ * @author 苍海之南
+ */
+public interface IFlyAnimalAction {
+	void fly();
+}
