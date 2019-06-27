ac497f9bceb4b833c2b2c9e58ca97d1a903098b0
CangHai <qc_0f13245b35f84d8bbe62d41e5a117dcd@git.code.tencent.com>
2019-06-27 18:34:05 +0800

    :rocket: Add：添加test模块

---
+++ java/spring-annotation/src/test/java/com/github/zj/dreamly/spring/annotation/ioc/ConfigTest.java
@@ -0,0 +1,31 @@
+package com.github.zj.dreamly.spring.annotation.ioc;
+
+import com.github.zj.dreamly.spring.annotation.BaseTest;
+import com.github.zj.dreamly.spring.annotation.bean.Person;
+import com.github.zj.dreamly.spring.annotation.config.TestConfig1;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * <h2>config test</h2>
+ *
+ * @author: 苍海之南
+ * @since: 2019-06-27 17:18
+ **/
+@Slf4j
+public class ConfigTest extends BaseTest {
+
+	@Test
+	public void test1() {
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TestConfig1.class);
+		Person bean = applicationContext.getBean(Person.class);
+		log.info("【person】：{}", bean);
+
+		String[] namesForType = applicationContext.getBeanNamesForType(Person.class);
+		for (String name : namesForType) {
+			log.info("【name】：{}", name);
+		}
+	}
+}
